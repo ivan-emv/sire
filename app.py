c612da449f1058f99a836435dc568ff6920f889f
ivan-emv <ivan.amador@europamundo.com>
2025-04-04 19:53:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ if modo == "📝 Carga de Incidencias":
     # --------- Búsqueda de Registros ---------
 elif modo == "🔍 Búsqueda de Registros":
     st.header("🔍 Consulta de Incidencias por Usuario y Localizador")
-    with st.expander("🔎 FILTROS", expanded=False):
+        with st.expander("🔎 FILTROS", expanded=False):
     st.write("Opciones de filtro")
 
     st.header("🔍 Consulta de Incidencias por Usuario y Localizador")
