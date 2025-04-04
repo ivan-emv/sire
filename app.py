5868a54a913a76645e754f9a0b362476a41a5a84
ivan-emv <ivan.amador@europamundo.com>
2025-04-04 18:54:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ RESOLUCIONES = [
 
 
 # --------- Inicializar sesión ---------
-def init_session():
+def init_session()
     if "incidencias" not in st.session_state:
         st.session_state.incidencias = []
     if "datos_generales" not in st.session_state:
@@ -23,7 +23,7 @@ def init_session():
     if "admin_autenticado" not in st.session_state:
         st.session_state.admin_autenticado = False
 
-init_session():
+init_session()
     if "incidencias" not in st.session_state:
         st.session_state.incidencias = []
     if "datos_generales" not in st.session_state:
