75e9d4bf536bdd636faeb9eaa794de38eedd8e8f
ivan-emv <ivan.amador@europamundo.com>
2025-04-04 20:32:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ if modo == "📝 Carga de Incidencias":
 elif modo == "🔍 Búsqueda de Registros":
     st.header("🔍 Consulta de Incidencias por Usuario y Localizador")
     with st.expander("🔎 FILTROS", expanded=False):
-    st.write("Opciones de filtro")
+        st.write("Opciones de filtro")
 
     st.header("🔍 Consulta de Incidencias por Usuario y Localizador")
 
@@ -426,7 +426,7 @@ elif modo == "🔍 Búsqueda de Registros":
 elif modo == "🛠️ Gestión de Registros" and st.session_state.admin_autenticado:
     st.header("🛠️ Gestión de Registros (Administrador)")
     with st.expander("🔎 FILTROS", expanded=False):
-    st.write("Opciones de filtro")
+        st.write("Opciones de filtro")
 
     st.header("🛠️ Gestión de Registros (Administrador)")
 
